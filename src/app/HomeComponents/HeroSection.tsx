4fd82b44922684b5e15efdff28667406f6d4dc6e
RajaHadi <shabanajawed411@gmail.com>
2025-01-24 11:46:52 -0800

    ho

--- src/app/HomeComponents/HeroSection.tsx
+++ src/app/HomeComponents/HeroSection.tsx
@@ -53,9 +53,10 @@ export default function HeroSection() {
         <p className="mt-4 text-gray-400">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam sed phasellus dictum neque.
         </p>
+        <Link href={"../Shop"}>
         <button className="mt-6 px-6 py-3 bg-[#FF9F0D] text-black font-semibold rounded-[30px] hover:bg-yellow-500">
           See Menu
-        </button>
+        </button></Link>
       </div>
   
       {/* Image */}
